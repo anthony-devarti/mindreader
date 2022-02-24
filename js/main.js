8559c404152fa83131ef1c97f7960e1234a423e0
Anthony DeVarti <anthony.devarti@student.awesomeinc.org>
2022-02-24 21:48:21 +0000

    working on random symbols

--- js/main.js
+++ js/main.js
@@ -24,28 +24,53 @@ class Page {
 //case break to differentiate between page numbers?
 
 
+let symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "+", "=", "|"]
+
+let nineSymbol = randomSymbol();
+
+function hide(id){
+    document.getElementById(id).style.display="none"
+}
+
+function reveal(id) {
+    document.getElementById(id).style.display="block"
+}
+
+function eventClear(button) {
+    document.getElementById(button).removeEventListener("click", page1);
+    document.getElementById(button).removeEventListener("click", page2);
+    document.getElementById(button).removeEventListener("click", page3);
+    document.getElementById(button).removeEventListener("click", page4);
+    document.getElementById(button).removeEventListener("click", page5);
+    document.getElementById(button).removeEventListener("click", page6);
+};
+
+let arr=[]
 
 function page1() {
+    hide("next");
+    hide("helper");
+    hide("scroll");
     let headerText = document.getElementById("headerText");
     headerText.innerHTML = "I can read your mind";
-    let next = document.getElementById("next").style.display='none';
-    let helper = document.getElementById("helper").style.display='none';
     let nav = document.getElementById("nav");
     nav.innerHTML = "Go";
-    document.getElementById("nav").removeEventListener("click", page1);
+    eventClear("nav")
     document.getElementById("nav").addEventListener("click", page2);
 }
 
 function page2() {
     let headerText = document.getElementById("headerText");
     headerText.innerHTML = "Pick a number from 01-99";
-    let next = document.getElementById("next");
-    next.innerHTML = "Next"
-    document.getElementById("next").addEventListener("click", page3)
-    let helper = document.getElementById("helper")
-    helper.innerHTML = "when you have your number click next"
+    reveal("next");
+    next.innerHTML = "Next";
+    document.getElementById("next").removeEventListener("click", page2)
+    document.getElementById("next").addEventListener("click", page3);
+    reveal("helper");
+    helper.innerHTML = "When you have your number, click next";
     let nav = document.getElementById("nav")
     nav.innerHTML = "Return";
+    eventClear("nav");
     document.getElementById("nav").addEventListener("click", page1)
 }
 
@@ -53,12 +78,15 @@ function page3() {
     let headerText = document.getElementById("headerText")
     headerText.innerHTML = "Add both digits together to get a new number"
     let next = document.getElementById("next")
+    reveal("next");
     next.innerHTML = "Next"
+    document.getElementById("next").removeEventListener("click", page3)
     document.getElementById("next").addEventListener("click", page4)
     let helper = document.getElementById("helper")
     helper.innerHTML = "ex: 14 is 1+4=5"
     let nav = document.getElementById("nav")
     nav.innerHTML = "Return";
+    eventClear("nav");
     document.getElementById("nav").addEventListener("click", page1)
 }
 
@@ -66,38 +94,95 @@ function page4() {
     let headerText = document.getElementById("headerText")
     headerText.innerHTML = "Subtract your new number from the original number"
     let next = document.getElementById("next")
+    reveal("next");
     next.innerHTML = "Next"
+    eventClear("next");
     document.getElementById("next").addEventListener("click", page5)
     let helper = document.getElementById("helper")
     helper.innerHTML = "Ex: 14-5=9"
     let nav = document.getElementById("nav")
     nav.innerHTML = "Return";
+    eventClear("nav");
     document.getElementById("nav").addEventListener("click", page1)
 }
 
 function page5() {     
-    let headerText = document.getElementById("headerText")
-    headerText.innerHTML = "Symbol Map" //replace this with the functioning scroller built earlier
+    hide("headerText")
+    reveal("scroll")
+    generateArr();
+    let scroll = document.getElementById("scroll")
+    scroll.innerHTML = arr.toString() //replace this with the functioning scroller built earlier. maybe have it reveal a scroll box that was always there. 
     let next = document.getElementById("next")
     next.innerHTML = "Reveal"
+    eventClear("next");
     document.getElementById("next").addEventListener("click", page6)
     let helper = document.getElementById("helper")
     helper.innerHTML = "Find your new number. Note the symbol beside the number"
     let nav = document.getElementById("nav")
     nav.innerHTML = "Return";
+    eventClear("nav");
     document.getElementById("nav").addEventListener("click", page1)
 }
 
 function page6(){
+    hide("scroll");
+    reveal("headerText");
     let headerText = document.getElementById("headerText")
     headerText.innerHTML = "&"
-    let next = document.getElementById("next").style.display="hide"
+    hide("next")
     let helper = document.getElementById("helper")
     helper.innerHTML = "Your symbol is: &"
     let nav = document.getElementById("nav")
     nav.innerHTML = "Return";
+    eventClear("nav");
     document.getElementById("nav").addEventListener("click", page1)
 }
+
+function shuffle(array){
+    let m = array.length, t, i;
+    while (m) {
+        i = Math.floor(Math.random() * m--);
+
+        t = array(m);
+        array[m] = array[i];
+        array[i] = t
+    }
+    return array
+}
+
+function mod9(x) {
+    if (x%9 ===0)
+    return true
+}
+
+function generateArr() {
+    for (i=0; i<100; i++) {
+        arr.push(i);
+    }
+    //something here that writes random symbols next to everything.
+    //replace the @ symbol with the result of the random symbol
+    
+    arr = arr.map(i => "\n" + randomSymbol() + i);
+    
+    //something here that writes a random symbol to all numbers divisible by 9.
+    // for (element of arr) {
+    //     if (element%9===0){
+            
+    //     }
+        
+    // }
+}
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+function randomSymbol(){
+    let result = getRandomInt(14);
+    return symbols[result];
+}
+
+
 
 page1();
 
